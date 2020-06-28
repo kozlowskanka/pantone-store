7c7fe39264b546a6b561f6bfcee149ba49490165
Anka Kozłowska <kozlowskanka@gmail.com>
2020-06-28 17:03:28 +0200

    Improve the form

--- src/components/views/Form/Form.js
+++ src/components/views/Form/Form.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
-import { getCart, saveOrder } from '../../../redux/cartRedux.js';
+import { getCart, saveOrder, emptyTheCart } from '../../../redux/cartRedux.js';
 
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { Title } from '../../common/Title/Title';
+import { Link } from 'react-router-dom';
 
+import { Title } from '../../common/Title/Title';
 import { Button } from '../../common/Button/Button';
 
 import styles from './Form.module.scss';
 
-const Component = ({cart, saveOrder}) => {
+const Component = ({cart, saveOrder, emptyTheCart }) => {
 
   const [order, newOrder] = React.useState({
     name: '',
@@ -33,80 +34,95 @@ const Component = ({cart, saveOrder}) => {
     e.preventDefault();
     console.log('about to saveee');
     saveOrder(order);
+    emptyTheCart();
   };
 
   return (
     <div className={styles.component}>
       <Grid>
         <Title name='Order details'/>
-        <form onSubmit={e => handleSubmit(e)}>
-          <Row className={styles.formLine}>
-            <Col className={styles.label} xs={12}>
-              <label>First Name</label>
+        {cart.length>0
+          ?
+          <form onSubmit={e => handleSubmit(e)}>
+            <Row className={styles.formLine}>
+              <Col className={styles.label} xs={12}>
+                <label>First Name</label>
+              </Col>
+              <Col className={styles.input} xs={12}>
+                <input
+                  type="text"
+                  name="name"
+                  onChange={e => handleChange(e, 'name')}></input>
+              </Col>
+            </Row>
+            <Row className={styles.formLine}>
+              <Col className={styles.label} xs={12}>
+                <label>Last Name</label>
+              </Col>
+              <Col className={styles.input} xs={12}>
+                <input
+                  type="text"
+                  name="surname"
+                  onChange={e => handleChange(e, 'surname')}>
+                </input>
+              </Col>
+            </Row>
+            <Row className={styles.formLine}>
+              <Col className={styles.label} xs={12}>
+                <label>E-mail</label>
+              </Col>
+              <Col className={styles.input} xs={12}>
+                <input
+                  type="text"
+                  name="mail"
+                  onChange={e => handleChange(e, 'mail')}></input>
+              </Col>
+            </Row>
+            <Row className={styles.formLine}>
+              <Col className={styles.label} xs={12}>
+                <label>Address</label>
+              </Col>
+              <Col className={styles.input} xs={12}>
+                <input
+                  type="text"
+                  name="address"
+                  onChange={e => handleChange(e, 'address')}>
+                </input>
+              </Col>
+            </Row>
+            <Row className={styles.formLine}>
+              <Col className={styles.label} xs={12}>
+                <label>Phone</label>
+              </Col>
+              <Col className={styles.input} xs={12}>
+                <input
+                  type="text"
+                  name="phone"
+                  onChange={e => handleChange(e, 'phone')}>
+                </input>
+              </Col>
+            </Row>
+            <Col className={styles.submit} xs={12}>
+              <Button
+                variant='dark'
+                align = "center"
+                type="submit">
+                    SEND
+              </Button>
             </Col>
-            <Col className={styles.input} xs={12}>
-              <input
-                type="text"
-                name="name"
-                onChange={e => handleChange(e, 'name')}></input>
-            </Col>
-          </Row>
-          <Row className={styles.formLine}>
-            <Col className={styles.label} xs={12}>
-              <label>Last Name</label>
-            </Col>
-            <Col className={styles.input} xs={12}>
-              <input
-                type="text"
-                name="surname"
-                onChange={e => handleChange(e, 'surname')}>
-              </input>
-            </Col>
-          </Row>
-          <Row className={styles.formLine}>
-            <Col className={styles.label} xs={12}>
-              <label>E-mail</label>
-            </Col>
-            <Col className={styles.input} xs={12}>
-              <input
-                type="text"
-                name="mail"
-                onChange={e => handleChange(e, 'mail')}></input>
-            </Col>
-          </Row>
-          <Row className={styles.formLine}>
-            <Col className={styles.label} xs={12}>
-              <label>Address</label>
-            </Col>
-            <Col className={styles.input} xs={12}>
-              <input
-                type="text"
-                name="address"
-                onChange={e => handleChange(e, 'address')}>
-              </input>
-            </Col>
-          </Row>
-          <Row className={styles.formLine}>
-            <Col className={styles.label} xs={12}>
-              <label>Phone</label>
-            </Col>
-            <Col className={styles.input} xs={12}>
-              <input
-                type="text"
-                name="phone"
-                onChange={e => handleChange(e, 'phone')}>
-              </input>
-            </Col>
-          </Row>
-          <Col className={styles.submit} xs={12}>
-            <Button
-              variant='dark'
-              align = "center"
-              type="submit">
-              SEND
-            </Button>
-          </Col>
-        </form>
+          </form>
+          :
+          <div className={styles.info}>
+            <h2>Fill your cart with colors</h2>
+            <Link to='/'>
+              <Button
+                variant='dark'
+                align = "center">
+                  Check more PANTONES<sup>&reg;</sup>
+              </Button>
+            </Link>
+          </div>
+        }
       </Grid>
     </div>
   );
@@ -116,6 +132,7 @@ const Component = ({cart, saveOrder}) => {
 Component.propTypes = {
   saveOrder: PropTypes.func,
   cart: PropTypes.array,
+  emptyTheCart: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
@@ -124,6 +141,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   saveOrder: order => dispatch(saveOrder(order)),
+  emptyTheCart: () => dispatch(emptyTheCart()),
 });
 
 const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -15,6 +15,7 @@ const FETCH_ERROR = createActionName('FETCH_ERROR');
 
 const ADD_TO_CART = createActionName('ADD_TO_CART');
 const REMOVE_FROM_CART = createActionName('REMOVE_FROM_CART');
+const EMPTY_THE_CART = createActionName('EMPTY_THE_CART');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
@@ -23,6 +24,7 @@ export const fetchError = payload => ({ payload, type: FETCH_ERROR });
 
 export const addToCart = payload => ({ payload, type: ADD_TO_CART });
 export const removeFromCart = payload => ({ payload, type: REMOVE_FROM_CART });
+export const emptyTheCart = payload => ({ payload, type: EMPTY_THE_CART });
 
 /* thunk creators */
 
@@ -46,37 +48,32 @@ export const reducer = (statePart = [], action = {}) => {
     case FETCH_START: {
       return {
         ...statePart,
-        // loading: {
-        //   active: true,
-        //   error: false,
-        // },
       };
     }
     case FETCH_SUCCESS: {
       return {
         ...statePart,
-        // loading: {
-        //   active: false,
-        //   error: false,
-        // },
         data: action.payload,
       };
     }
     case FETCH_ERROR: {
       return {
         ...statePart,
-        // loading: {
-        //   active: false,
-        //   error: action.payload,
-        // },
       };
     }
 
-    case ADD_TO_CART:
+    case ADD_TO_CART: {
       return [...statePart, action.payload];
+    }
 
-    case REMOVE_FROM_CART:
+    case REMOVE_FROM_CART: {
+      console.log('remove');
       return statePart.filter(products => products.id !== action.payload);
+    }
+
+    case EMPTY_THE_CART: {
+      return statePart = [];
+    }
 
     default:
       return statePart;
